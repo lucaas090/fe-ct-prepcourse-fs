e2ff87bdbcb9bea8d0bdf74ce681b4c30b5a74fc
Lucaas090 <lucasparedes571@gmail.com>
2023-03-11 16:23:12 -0300

    Tarea resuelta

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,7 +6,20 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
-   
+   constructor(nombre,apellido,edad, domicilio){
+      this.nombre= nombre
+         this.apellido= apellido
+         this.edad= edad
+         this.domicilio= domicilio;
+   }
+   detalle(){
+      return {
+         nombre: this.nombre,
+         apellido: this.apellido,
+         edad: this.edad,
+         domicilio: this.domicilio,
+      }
+   }
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
@@ -14,14 +27,17 @@ function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
-}
+   return new Persona(nombre, apellido, edad, domicilio)
+ }
 
 function agregarMetodo() {
    // La función agrega un método "datos" a la clase "Persona".
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
-
+   Persona.prototype.datos= function(){
+      return this.nombre+ ', ' + this.edad + ' años';
+   }
 }
 
 
--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -6,13 +6,14 @@ function mayuscula(nombre) {
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
    
-}
+  return nombre[0].toUpperCase()+ nombre.slice(1)
 
+}
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
- 
+ cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -20,7 +21,8 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
-  
+   var res = cb(num1, num2)
+   return res;
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -29,7 +31,11 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
-  
+  var suma= 0
+  for(let i =0; i<arrayOfNumbers.length; i++){
+   suma+=arrayOfNumbers[i]
+  }
+  cb(suma);
 }
 
 function forEach(array, cb) {
@@ -37,7 +43,9 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
-  
+  for(let i=0; i<array.length; i++){
+      cb(array[i]) 
+  }
 }
 
 function map(array, cb) {
@@ -45,13 +53,24 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
-   
-}
+   var newArr= []
+   array.forEach((element) =>{
+      newArr.push(cb(element));
+   })
+   return newArr;
+  }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   var array=[]
+   arrayOfStrings.forEach((element)=>{
+      if(element[0]=== 'a'){
+         array.push(element);
+      }
+   })
+   return array;
 }
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
